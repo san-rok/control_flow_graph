a13698bad69b5e7bb35d64fdda24516e148bba02
Sándor Rokob <sr@zeroitlab.com>
2023-08-29 11:48:39 +0200

    modify: lib.rs - main.rs separation is done (correctly this time)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+
+use control_flow_graph::*;
+
 fn main() {
-    println!("Hello, world!");
+    
+    let path = String::from("/home/san-rok/projects/testtest/target/debug/testtest");
+    let binary = Binary::from_elf(path);
+
+    let virtual_address: u64 =  0x96b4;
+    // test: 0x88cb, 0x8870, 0x88b0, 0x8a0d, 0x893e, 0x88f0, 0x8c81, 0x8840, 0x8f41, 0x970b, 0x96b4
+
+    let cfg: ControlFlowGraph = ControlFlowGraph::from_address(&binary, virtual_address);
+
+    let mut f = std::fs::File::create("/home/san-rok/projects/virtual_address/virtual_address.dot").unwrap();
+    cfg.render_to(&mut f).unwrap();
+    // dot -Tsvg virtual_address.dot > virtual_address.svg
+
+    
 }
